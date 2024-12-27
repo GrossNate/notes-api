243a259657b4c423afbdf972741ba236656832f6
Nathan Gross <117976176+GrossNate@users.noreply.github.com>
2024-12-26 22:03:30 -0600

    Delete build directory

--- build/src/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.safeAssertString = safeAssertString;
-function safeAssertString(val) {
-    if (typeof val !== "string") {
-        throw new Error("Unexpected type. Expected string.");
-    }
-}
--- build/src/utils/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Note that we're using dotenvx and encryption for all env variables.
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TOKEN_SECRET = exports.MONGODB_URI = exports.API_PORT = void 0;
-exports.API_PORT = process.env.API_PORT;
-exports.MONGODB_URI = process.env.MONGODB_URI;
-exports.TOKEN_SECRET = process.env.TOKEN_SECRET;
